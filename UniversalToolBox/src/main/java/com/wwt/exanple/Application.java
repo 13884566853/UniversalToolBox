e766d96a880f174908687c9eac7eb266a99d8150
fireeck <838924569@qq.com>
2022-06-25 13:46:00 +0800

    搭建最基本的springboot

---
+++ UniversalToolBox/src/main/java/com/wwt/exanple/Application.java
@@ -0,0 +1,18 @@
+package com.wwt.exanple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wwt
+ * @title: Application
+ * @description: TODO
+ * @date 2022/6/25 13:43
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+
+    }
+}
